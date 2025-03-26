d3aba2f74d49f1aca12aa2ae94b50b7f48556c9e
brunodimarco <bruno.dimarco@outlook.com>
2025-03-26 05:12:45 +0100

    switch

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
   plugins: [react()],
+  base: "/", // 👈 essenziale per GitHub Pages su root domain
 });
